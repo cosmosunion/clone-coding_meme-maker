c70bbbd28369725b36cf5fd3f49e6a9e7e79f2c7
cosmosunion <105220575+cosmosunion@users.noreply.github.com>
2022-09-25 15:06:51 +0900

    canvas api

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const canvas = document.querySelector("canvas");
-
-/**캔버스에 그림을 그릴때 사용 */
 const ctx = canvas.getContext("2d");
-d;
-/** 선 굵기 input값 */
-const lineWidthInput = document.querySelector("#line-width");
-/*canvas에서 이미지의 퀄리티를 높이기 위해 width/height를 javascript에서만 수정*/
-canvas.width = 800;
-canvas.height = 800;
-ctx.lineWidth = lineWidthInput.value;
+const lineWidth = document.getElementById("line-width");
 
-let draw = false;
+canvas.width = 600;
+canvas.height = 600;
+
+let isPainting = false;
+
 function startPainting() {
-  draw = true;
+  isPainting = true;
+  console.log("painting");
 }
-function cancelPainting() {
-  draw = false;
+
+function stopPainting() {
+  isPainting = false;
+  console.log("stop painting");
 }
-/**선 그리기 */
+
 function onMove(event) {
-  if (draw) {
+  if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
+    return;
   }
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
-canvas.addEventListener("mouseup", cancelPainting);
-canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mouseleave", stopPainting);
